28697d6920e3a268ea07da0c391ac5710ef76a00
omtians9425 <rskowgshakhtardonetsk32517@gmail.com>
2021-03-02 22:56:32 +0900

    Add thumbnail and backkey to detail screen

--- app/src/main/java/com/compose/omtians9425/puppyadoption/ui/screens/PuppyDetailScreen.kt
+++ app/src/main/java/com/compose/omtians9425/puppyadoption/ui/screens/PuppyDetailScreen.kt
@@ -1,8 +1,36 @@
-import androidx.compose.material.TextField
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.height
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import com.compose.omtians9425.puppyadoption.ui.screens.puppies
+import dev.chrisbanes.accompanist.coil.CoilImage
 
 @Composable
 fun PuppyDetailScreen(navController: NavController) {
-    TextField(value = "Detail", onValueChange = { /*TODO*/ })
+    Column {
+        Box(modifier = Modifier.height(200.dp)) {
+            CoilImage(
+                data = puppies.first().imageUrl,
+                contentDescription = puppies.first().name,
+                fadeIn = true,
+                contentScale = ContentScale.Crop,
+            )
+            IconButton(onClick = { navController.popBackStack() }) {
+                Icon(
+                    Icons.Default.ArrowBack,
+                    contentDescription = "back",
+                    tint = Color.White
+                )
+            }
+        }
+    }
 }
