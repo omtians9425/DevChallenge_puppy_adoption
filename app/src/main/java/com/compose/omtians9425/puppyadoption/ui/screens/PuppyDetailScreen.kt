007ba262722d20892462d06c506a9eae3722c35f
omtians9425 <rskowgshakhtardonetsk32517@gmail.com>
2021-03-02 23:29:13 +0900

    Show BottomSheet like Box

--- app/src/main/java/com/compose/omtians9425/puppyadoption/ui/screens/PuppyDetailScreen.kt
+++ app/src/main/java/com/compose/omtians9425/puppyadoption/ui/screens/PuppyDetailScreen.kt
@@ -1,12 +1,17 @@
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
@@ -14,10 +19,11 @@ import androidx.navigation.NavController
 import com.compose.omtians9425.puppyadoption.ui.screens.puppies
 import dev.chrisbanes.accompanist.coil.CoilImage
 
+@OptIn(ExperimentalMaterialApi::class)
 @Composable
 fun PuppyDetailScreen(navController: NavController) {
-    Column {
-        Box(modifier = Modifier.height(200.dp)) {
+    Box {
+        Box(modifier = Modifier.height(250.dp)) {
             CoilImage(
                 data = puppies.first().imageUrl,
                 contentDescription = puppies.first().name,
@@ -32,5 +38,12 @@ fun PuppyDetailScreen(navController: NavController) {
                 )
             }
         }
+        Box(
+            modifier = Modifier
+                .offset(y = 200.dp)
+                .clip(RoundedCornerShape(32.dp))
+                .background(color = Color(0xFF5a65fa))
+                .fillMaxSize(),
+        )
     }
 }
